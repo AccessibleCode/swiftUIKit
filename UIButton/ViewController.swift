9c29352ffbe62b2b792abec6214329274b19a789
Всеволод <mixseva@gmail.com>
2022-12-06 20:11:07 +0300

    first red button

--- UIButton/ViewController.swift
+++ UIButton/ViewController.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // первая красная кнопка
+    var firstRedButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        createFirstRedButton()
+    }
+    
+    
+    // MARK: - Методы создания кнопки
+    
+    // первая красная кнопка
+    func createFirstRedButton() {
+        // инициализатор и тип кнопки
+        self.firstRedButton = UIButton(type: .roundedRect)
+        // задать координаты для рисования кнопки - frame
+        self.firstRedButton.frame = CGRect(x: 30, y: 100, width: 150, height: 70)
+        // задать надпись для кнопки
+        self.firstRedButton.setTitle("1ая", for: .normal)
+        // задать цвет кнопки
+        self.firstRedButton.backgroundColor = .red
+        // задать цвет текста
+        self.firstRedButton.setTitleColor(.white, for: .normal)
+        // добавить кнопку на view
+        self.view.addSubview(self.firstRedButton)
     }
 
 
