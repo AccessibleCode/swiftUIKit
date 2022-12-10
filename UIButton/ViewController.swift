d7578121c7cf85b274760be3b9f206807ab9db3b
Всеволод <mixseva@gmail.com>
2022-12-10 20:45:08 +0300

    arrayButton and secondGreenButton

--- UIButton/ViewController.swift
+++ UIButton/ViewController.swift
@@ -9,19 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // первая красная кнопка
+    // 1ая красная кнопка
     var firstRedButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         createFirstRedButton()
+        customArrayButton()
+        createSecondGreenButton()
     }
+    
+    // массив из 3х кнопок
+    //  1 массив желтая - тег 1
+    //  2 массив оранжевая - тег 2
+    //  3 массив фиолетовая -  тег 3
+    @IBOutlet var arrayButton: [UIButton]!
+    
+    // 2ая зеленая кнопка
+    var secondGreenButton = UIButton()
     
     
     // MARK: - Методы создания кнопки
     
-    // первая красная кнопка
+    // 1ая красная кнопка
     func createFirstRedButton() {
         // инициализатор и тип кнопки
         self.firstRedButton = UIButton(type: .roundedRect)
@@ -35,6 +46,54 @@ class ViewController: UIViewController {
         self.firstRedButton.setTitleColor(.white, for: .normal)
         // добавить кнопку на view
         self.view.addSubview(self.firstRedButton)
+    }
+    
+    
+    
+    // Массив из 3х кнопок
+    // прочитать массив кнопок циклом for
+    // задать кастоиные параметры для каждой кнопки
+    func customArrayButton() {
+        for button in arrayButton {
+            button.setTitleColor(UIColor.black, for: .normal)
+    }
+
+    }
+    
+    // Отслеживать кнопку и применять действие из массива кнопок
+    @IBAction func arrayButtonAction(_ sender: Any) {
+        let button = sender as! UIButton
+        
+        if button.tag == 1 {
+            print("Button number is 1")
+        } else if button.tag == 2 {
+            print("Button number is 2")
+        } else if button.tag == 3 {
+            print("Button number is 3")
+        }
+        
+        if button.titleLabel?.text == "1 массив" {
+            print("Ты нажал на кнопку номер 1")
+        }
+        
+    }
+    
+    // 2ая зеленая кнопка
+    func createSecondGreenButton() {
+        // нарисовать зону, где будет отображаться UIButton
+        secondGreenButton.frame = CGRect(x: view.bounds.size.width / 4 + 30,
+                              y: view.center.y + 100,
+                              width: 150, height: 50)
+        // название и состояние кнопки
+        // взята из UIProgressView - "Загрузка..."
+        secondGreenButton.setTitle("2ая", for: .normal)
+        // цвет названия кнопки и состояние
+        secondGreenButton.setTitleColor(.black, for: .normal)
+        // цвет кнопки
+        secondGreenButton.backgroundColor = UIColor.green
+        // прозрачность кнопки
+        secondGreenButton.alpha = 0.5
+        view.addSubview(secondGreenButton)
     }
 
 
